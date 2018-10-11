707484b0c1868bc9d5a5f904fd42d588d4651dea
tchoedak <tchoedak@gmail.com>
2018-10-10 23:19:38 -0700

    init db

--- anoti/db.py
+++ anoti/db.py
@@ -1,2 +1,15 @@
 import sqlite3
-import sqlalchemy
+import config
+from sqlalchemy import engine, create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+_db = f"{config.app}.db"
+engine = create_engine(f'sqlite:///{_db}')
+_sessionmaker = sessionmaker(bind=engine)
+session = _sessionmaker()
+
+def create_db():
+    conn = sqlite3.connect(f"{config.app}.db")
+    print(sqlite3.version)
+
