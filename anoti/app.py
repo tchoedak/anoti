bd64617c241d42ccdc29ca60a2bd47bf44d19d28
tchoedak <tchoedak@gmail.com>
2018-11-11 23:06:16 -0800

    update main entrypoint to pulse amazon orders

--- anoti/app.py
+++ anoti/app.py
@@ -1,9 +1,21 @@
 import mws
-from datetime import datetime
+from datetime import datetime, timedelta
 import click
-from anoti import orders, config, db
+from anoti import orders, config, db, rules
 
 
 @click.command()
 def cli():
-    pass
+    pulse()
+
+def pulse():
+    pulse_range = timedelta(hours=24)
+    pulse_interval = timedelta(hours=1)
+    pulse_orders = orders.orders(last_updated_after=datetime.now() - pulse_range)
+    for order in pulse_orders:
+        if all([rule(order) for rule in rules.rules]):
+            alert(order)
+    pulse_orders.save()
+
+def alert(order):
+    print('Woah!!!')
