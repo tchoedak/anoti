b2eb5beb06506718117f1816139ac594ced6d10e
tchoedak <tchoedak@gmail.com>
2018-11-11 23:11:48 -0800

    add query to check if order exists already

--- anoti/queries.py
+++ anoti/queries.py
@@ -2,17 +2,26 @@ from . import models
 from . import db
 
 
-def new_orders(order_ids):
+def new_orders(orders):
     """
-    Returns order_ids from ``order_ids` if they don't exist in the DB
+    Returns orders that haven't already been registered in the DB
     """
-    existing_orders = (
+    order_ids = [order.amazon_order_id for order in orders]
+    new_orders = (
         db.session.query(models.Order)
-        .filter(models.Order.amazon_order_id.in_(order_ids))
-        .all()
+        .filter(models.Order.amazon_order_id.notin(order_ids))
     )
-
-    existing_order_ids = [order.amazon_order_id for order in existing_orders]
-
-    new_orders = list(set(existing_orders).difference(set(order_ids)))
     return new_orders
+
+def exists(order):
+    """
+    Returns True if the order exists in the DB
+    """
+    order = (
+        db.session.query(models.Order)
+        .filter(models.Order.amazon_order_id.in(order.amazon_order_id))
+    ).first()
+    if order:
+        return True
+    else:
+        return False
