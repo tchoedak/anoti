65bd009e69ac66110c563dcf3d247cd13a7c808c
tchoedak <tchoedak@gmail.com>
2018-11-11 23:21:39 -0800

    add rules

---
+++ anoti/rules.py
@@ -0,0 +1,24 @@
+from . import queries
+
+def is_status(order, status):
+    return order.OrderStatus == status
+
+def is_new(order):
+    """
+    order must not already be in the DB
+    """
+    return queries.exists(order) == False
+
+def is_pending(order):
+    """
+    Order must be in pending status
+    """
+    return is_status(order, 'Pending')
+
+def is_shipped(order):
+    """
+    Order must be in shipped status
+    """
+    return is_status(order, 'Shipped')
+
+rules = [is_new, is_shipped]
