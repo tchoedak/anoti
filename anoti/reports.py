f82dde5d608354f4cf8f5d4bcb99b0585851ec9c
tchoedak <tchoedak@gmail.com>
2018-11-25 15:51:51 -0800

    Sort by purchase date

--- anoti/reports.py
+++ anoti/reports.py
@@ -1,12 +1,12 @@
 from beautifultable import BeautifulTable
-
+from . import util
 
 def print_orders(*orders):
     print("""
     \033[1;32m====================================================================="""
     )
     table = BeautifulTable()
-    table.column_headers = ['Order Id', 'Order Type', 'Title', 'Order Amount', 'Purchase date']
+    table.column_headers = ['Order Id', 'Order Type', 'Title', 'Order Amount', 'Purchase Date']
     for order in orders:
         table.append_row(
             [
@@ -14,9 +14,10 @@ def print_orders(*orders):
                 order.OrderType,
                 order.OrderItem.Title,
                 order.OrderTotal.Amount if 'OrderTotal' in order.keys() else 'N/A',
-                order.PurchaseDate
+                order.PurchaseDate,
             ]
         )
 
+    table.sort('Purchase Date')
     print(table)
 
