41fec82d4c59e175660576c0371a067ed2eff12f
tchoedak <tchoedak@gmail.com>
2018-12-07 00:04:27 -0800

    make new sms configs available during deployment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,11 @@ echo "SMTP_PASSWORD=$SMTP_PASSWORD" >> /etc/environment
 echo "SELLER_CENTRAL_ACCESS_KEY_ID=$SELLER_CENTRAL_ACCESS_KEY_ID" >> /etc/environment
 echo "SELLER_CENTRAL_SECRET_KEY=$SELLER_CENTRAL_SECRET_KEY" >> /etc/environment
 echo "RECEIVER_EMAIL=$RECEIVER_EMAIL" >> /etc/environment
+echo "ACCOUNT_SID=$ACCOUNT_SID" >> /etc/environment
+echo "AUTH_TOKEN=$AUTH_TOKEN" >> /etc/environment
+echo "RECEIVER_NUMBER=$RECEIVER_NUMBER" >> /etc/environment
+echo "ANOTI_NUMBER=$ANOTI_NUMBER" >> /etc/environment
+
 source venv/bin/activate
 cd /root/app
 
