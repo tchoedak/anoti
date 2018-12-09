21367206de6c5b7ae4798036b006b87735592aec
tchoedak <tchoedak@gmail.com>
2018-12-08 22:54:23 -0800

    fix really stupid bug caused by overriding variable name

--- anoti/sms.py
+++ anoti/sms.py
@@ -3,9 +3,11 @@ from . import config
 
 
 def send_text_message(message):
+    client = Client(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN)
+
     print(f'Sending the following message to sms: {message}')
     for receiver_number in config.RECEIVER_NUMBER:
         client = Client(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN)
-        message = client.messages.create(
+        _message = client.messages.create(
             to=receiver_number, from_=config.ANOTI_NUMBER, body=message
         )
