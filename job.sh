05469f3d0fc9a121bde7fe436888f48fa3a9cf48
tchoedak <tchoedak@gmail.com>
2019-03-04 21:33:10 -0800

    update job

--- job.sh
+++ job.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 APP_HOME=/root/app
 . $HOME/.profile
+. /root/app/.env
 . /venv/bin/activate
 cd $APP_HOME
 export PYTHONPATH=$APP_HOME
-python anoti/app.py
+python anoti/app.py pulse 60 &
